ba4c51970a9854adcf125e84362454245fa515c5
apiaryfyf <150634691+apiaryfyf@users.noreply.github.com>
2023-12-18 11:34:56 -0600

    Update app.js

--- app.js
+++ app.js
@@ -56,6 +56,14 @@ const html = `
     <section>
       Hello from Render Ass!
     </section>
+    <form action="/action_page.php">
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value=""><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value=""><br><br>
+  <input type="submit" value="Submit">
+</form>
+    
   </body>
 </html>
 `
