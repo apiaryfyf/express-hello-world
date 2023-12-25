0ea5a12c5ccce74a1a6f619fdaeaf5a2b9e79bf0
apiaryfyf <150634691+apiaryfyf@users.noreply.github.com>
2023-12-25 13:15:26 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // added by us
-//app.use(express.static('public')); /* THIS SHIT DOESNT WORK */
-app.use(express.static(__dirname+ '/public'));
+app.use(express.static('public')); /* THIS SHIT DOESNT WORK */
+//app.use(express.static(__dirname+ '/public'));
 
 app.get("/", (req, res) => res.type('html').send(html));
 
