6b60210e08e58fff40f3e4954dd67b4e533be8c6
apiaryfyf <150634691+apiaryfyf@users.noreply.github.com>
2023-12-25 12:18:45 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+// added by us
+app.use(express.static('public'));
+
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
